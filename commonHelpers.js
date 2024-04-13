479f171ad949b9db763c5f6851cfed01c74d37ed
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-04-13 19:59:45 +0000

    Deploying to gh-pages from @ IhorDid/goit-js-hw-12@035332f7c242554c2ec6a5eb3a8e43b9c48fd4c6 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,6 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&l(a)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function l(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();function c(i){const t="https://pixabay.com/api/",s="42170319-af092c1d236dd53a733e41db9",l=new URLSearchParams({key:s,q:i,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${t}?${l}`).then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()})}function u(i){return i.map(({downloads:t,views:s,comments:l,likes:e,webformatURL:r,tags:a,largeImageURL:o})=>`<li class="gallery_item">
-        <a class="gallery_link" href="${o}">
-          <img class="gallery_img" src="${r}" data-sourse="${o}" alt="${a}" />
+import{a as c,i as u}from"./assets/vendor-632f698a.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();async function g(i,t=1){const s="https://pixabay.com/api/",a="42170319-af092c1d236dd53a733e41db9",e=new URLSearchParams({key:a,q:i,image_type:"photo",orientation:"horizontal",safesearch:!0,page:t,per_page:15}),{data:r}=await c.get(`${s}?${e}`);return r}function m(i){return i.map(({downloads:t,views:s,comments:a,likes:e,webformatURL:r,tags:o,largeImageURL:l})=>`<li class="gallery_item">
+        <a class="gallery_link" href="${l}">
+          <img class="gallery_img" src="${r}" data-sourse="${l}" alt="${o}" />
         </a>
         <ul class="gallery_item_params">
           <li class="gallery_item_params_link">
@@ -25,9 +25,9 @@
             <svg class="gallery_svg" width="24" height="24">
               <use href="../img/sprite.svg#icon-form-search"></use>
             </svg>
-            ${l}
+            ${a}
           </li>
           </ul>
           </li>
-          `).join("")}const n={searchForm:document.querySelector("#present_form"),list:document.querySelector(".gallery")};n.searchForm.addEventListener("submit",g);function g(i){i.preventDefault();const t=i.currentTarget.elements.fieldSearch;c(t.value).then(s=>{s.hits.length||iziToast.show({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topCenter",backgroundColor:"rgba(252, 249, 249, 1)",messageColor:"#000",zindex:"10000000"}),n.list.innerHTML=u(s.hits)}).catch(s=>console.log(s))}
+          `).join("")}const n={searchForm:document.querySelector("#present_form"),list:document.querySelector(".gallery"),loadBtn:document.querySelector(".js-load-more")};n.searchForm.addEventListener("submit",h);function h(i){i.preventDefault();const t=i.currentTarget.elements.fieldSearch;g(t.value,1).then(s=>{s.hits.length||u.show({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topCenter",backgroundColor:"rgba(252, 249, 249, 1)",messageColor:"#000",zindex:"10000000"}),n.list.innerHTML=m(s.hits)}).catch(s=>console.log(s))}
 //# sourceMappingURL=commonHelpers.js.map
